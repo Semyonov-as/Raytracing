cb7dbef80a6e7131218bd6d3e53620ec0da5735f
Alexandr Semenov <75676358+Semyonov-as@users.noreply.github.com>
2020-12-30 12:46:49 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include <src/Vec3.hpp>
+#include <src/Point3.hpp>
+#include <src/Color.hpp>
+#include <src/Ray.hpp>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
