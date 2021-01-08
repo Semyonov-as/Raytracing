a714bce2a5b11a2a9ecc38f6050c40001782839c
Alexandr <semyonov.as@phystech.edu>
2021-01-08 23:48:58 +0300

    deleted extra 3 from color

--- src/Color.hpp
+++ src/Color.hpp
@@ -3,17 +3,17 @@
 #include "Vector3.hpp"
 #include <iostream>
 
-using Color3D = Vector3D;
-using Color3F = Vector3F;
+using ColorD = Vector3D;
+using ColorF = Vector3F;
 
-void write_color(std::ostream &out, Color3D pixel_color) {
+void write_color(std::ostream &out, ColorD pixel_color) {
     // Write the translated [0,255] value of each color component.
     out << static_cast<int>(255.999 * pixel_color.x()) << ' '
         << static_cast<int>(255.999 * pixel_color.y()) << ' '
         << static_cast<int>(255.999 * pixel_color.z()) << '\n';
 }
 
-void write_color(std::ostream &out, Color3F pixel_color) {
+void write_color(std::ostream &out, ColorF pixel_color) {
     // Write the translated [0,255] value of each color component.
     out << static_cast<int>(255.999 * pixel_color.x()) << ' '
         << static_cast<int>(255.999 * pixel_color.y()) << ' '
