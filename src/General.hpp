28842f6f22b18e637b376b00713bc3a87bd338e3
Alexandr <semyonov.as@phystech.edu>
2021-01-11 20:48:03 +0300

    added random function

--- src/General.hpp
+++ src/General.hpp
@@ -3,6 +3,8 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <random>
+
 
 // Constants
 
@@ -14,3 +16,11 @@ const double pi = 3.1415926535897932385;
 double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
+
+template<typename T>
+T random(T min = 0.0, T max = 1.0) {
+    static std::uniform_real_distribution<T> distribution(min, max);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
